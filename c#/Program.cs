d7fdb9a34cb4e768e61c7081acf0df1e37b1eaa5
Johnny Herrera <jmjaramillo95@gmail.com>
2025-03-05 23:51:06 -0500

    Agregar archivos de proyecto.

---
+++ c#/Program.cs
@@ -0,0 +1,15 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+string nombre = "Pedro";
+int edad = 40;
+Console.WriteLine("Me llamo " + nombre + " y tengo " + edad + " años");
+Console.WriteLine("Tengo " + edad + " años y me llamo " + nombre);
+edad = edad + 1;
+Console.WriteLine("El año que viene tendrá " + edad + " años");
+
+int numeroEntero = 5;
+double numeroDecimal = 16.0;
+string texto = "lopju ";
+char letra = 'A';
+bool aprobado = true;
